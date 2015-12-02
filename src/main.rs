d6e5a423bb4f1822ae46dc53c743247890af3e0d
Kriegslustig <github@ls7.ch>
2015-12-02 23:14:05 +0100

    Write a working udp socket example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::net::UdpSocket;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = match UdpSocket::bind("127.0.0.1:3042") {
+        Err(e) => panic!("Error while binding to 3042/udp: {}", e),
+        Ok(res) => res
+    };
+    let mut buf = [0; 10];
+    match socket.recv_from(&mut buf) {
+        Err(e) => panic!("Error while reading incomming packet: {}", e),
+        Ok((_, _)) => {
+            let str = match str::from_utf8(&buf){
+                Err(e) => panic!("Error while pasing input: {}", e),
+                Ok(res) => res
+            };
+            print!("{:?}", str);
+        }
+    };
 }
+
